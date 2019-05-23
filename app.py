aad9642a02599ec3ac2ca444b76a86acca1b147a
Cody EB <49130314+NordicSplatter@users.noreply.github.com>
2019-05-22 20:08:54 -0500

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def home_page():
 @app.route('/<name>')
 def profile(name):
 	new_name = str(name) + " understands that a zen mind will forever be a student's mind"
-	return render_template('index.html', new_name=name)
+	return render_template('index.html', name=new_name)
 
 
 @app.route('/add_numbers', methods=['GET','POST'])
